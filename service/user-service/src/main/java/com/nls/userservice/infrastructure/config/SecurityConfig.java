1f8c2ba9378baab7de360cfd82a01d4451ddd0cf
aziky <pcm230304@gmail.com>
2025-05-22 15:32:38 +0700

    fix bug

--- service/user-service/src/main/java/com/nls/userservice/infrastructure/config/SecurityConfig.java
+++ service/user-service/src/main/java/com/nls/userservice/infrastructure/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.nls.userservice.infrastructure.config;
 
-import com.nls.common.enumration.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
