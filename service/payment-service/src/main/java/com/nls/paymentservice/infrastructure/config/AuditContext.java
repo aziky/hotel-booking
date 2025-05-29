5949870ffaf481b63e97b5cebe7f4f3be37bdd98
aziky <pcm230304@gmail.com>
2025-05-29 19:31:07 +0700

    add auditor for each user and feign

---
+++ service/payment-service/src/main/java/com/nls/paymentservice/infrastructure/config/AuditContext.java
@@ -0,0 +1,43 @@
+package com.nls.paymentservice.infrastructure.config;
+
+import com.nls.common.shared.CustomUserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class AuditContext {
+
+    private static final ThreadLocal<CustomUserDetails> temporaryUser = new ThreadLocal<>();
+
+    public void setTemporaryUser(String userId, String email, String role) {
+        List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(role));
+        CustomUserDetails userDetails = new CustomUserDetails(
+                UUID.fromString(userId), email, "", authorities
+        );
+        temporaryUser.set(userDetails);
+    }
+
+    public void setTemporaryUser(String email) {
+        List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("USER"));
+        CustomUserDetails userDetails = new CustomUserDetails(
+                UUID.fromString(UUID.randomUUID().toString()), email, "", authorities
+        );
+        temporaryUser.set(userDetails);
+    }
+
+    public CustomUserDetails getTemporaryUser() {
+        return temporaryUser.get();
+    }
+
+    public void clear() {
+        temporaryUser.remove();
+    }
+
+    public boolean hasTemporaryUser() {
+        return temporaryUser.get() != null;
+    }
+
+}
--- service/recommendation-service/src/main/java/com/nls/recommendationservice/infrastructure/config/SecurityConfig.java
+++ service/recommendation-service/src/main/java/com/nls/recommendationservice/infrastructure/config/SecurityConfig.java
@@ -2,18 +2,12 @@ package com.nls.recommendationservice.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
