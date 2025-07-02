357b99eca60ab399d5abe284d880d5330e807956
quocminhss1 <minhtqse183954@fpt.edu.vn>
2025-07-02 10:44:23 +0700

    add user and date to review

--- service/user-service/src/main/java/com/nls/userservice/api/dto/response/GetReviewRes.java
+++ service/user-service/src/main/java/com/nls/userservice/api/dto/response/GetReviewRes.java
@@ -1,10 +1,16 @@
 package com.nls.userservice.api.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record GetReviewRes  (
         UUID id,
         Integer rating,
-        String comment
+        String comment,
+        String createdBy,
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        LocalDateTime createdAt
 ){
 }
